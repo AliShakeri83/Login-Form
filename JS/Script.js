a2f507a64c884b78d3023f42f6a541939431444d
AliShakeri83 <ali.sh87873@gmail.com>
2024-12-23 21:03:39 +0330

    Update

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,13 @@
 let loginButtpn = document.getElementById('buttonLogin')
 let succsessful = document.querySelector('.succsessful')
+let username = document.querySelector('.username')
+let usernameEs = document.querySelector('.usernameEs')
 
 loginButtpn.addEventListener('click', (event) => {
     event.preventDefault()
     succsessful.style.display = 'block'
+})
+
+username.addEventListener('keyup', (e) => {
+    console.log(e);
 })
